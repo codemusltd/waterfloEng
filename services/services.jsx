c478d0d8998a894406fd17d1e86f057e7b4cfd51
Farai Tanekha <30661410+codemusltd@users.noreply.github.com>
2018-06-25 10:45:18 +0100

    solve service component rendering issue

--- services/services.jsx
+++ services/services.jsx
@@ -1,36 +1,32 @@
 export class Service extends React.Component{
+  
     render(){      
         return (
-          <div id='containerDiv' className={this.props.containerDivClassName}>
-              <h3 className={this.props.h3className}>{this.props.type}</h3>
-              <div>
-                <button className={this.props.sliderBtnClassName} onClick={this.prev}> Prev </button>  
-                <img id='slider' className={this.props.imgClassName} src={this.props.imgSrc} alt={this.props.imgAlt} width={this.props.imgWidth} height={this.props.imgHeight}/>
-                <button className={this.props.sliderBtnClassName} onClick={this.next}> Next</button>
-              </div>
-              <button className={this.props.dgBtnClassName}>Type</button>
-              <button className={this.props.blackBtnClassName}>{this.props.type}</button>
-              <button className={this.props.blackBtnClassName}>Length</button>
-              <button className={this.props.dgBtnClassName}>{this.props.length}</button>
-              <button className={this.props.dgBtnClassName}>Year</button>
-              <button className={this.props.blackBtnClassName}>{this.props.year}</button>
-          </div>
+            <div id='containerDiv' className={this.props.containerDivClassName}>
+                <div id='serviceImageContainer'>
+                    <button className={this.props.sliderBtnClassName} onClick={this.props.prev}> Prev </button>  
+                    <img id='serviceImage' className={this.props.imgClassName} src={this.props.imgSrc} alt={this.props.imgAlt} width={this.props.imgWidth} height={this.props.imgHeight}/>
+                    <button className={this.props.sliderBtnClassName} onClick={this.props.next}> Next</button>
+                </div>
+                <button className={this.props.dgBtnClassName}>Service</button>
+                <button className={this.props.blackBtnClassName}>{this.props.service}</button>
+                <button className={this.props.blackBtnClassName}>Year</button>
+                <button className={this.props.dgBtnClassName}>{this.props.year}</button>
+            </div>
       )
     }
 };
 
 Service.defaultProps={
-    imgSrc:'http://res.cloudinary.com/fatso/image/upload/v1523743969/Waterflo/services/1.jpg',
+    imgSrc:'http://res.cloudinary.com/fatso/image/upload/v1523744298/Waterflo/services/27.jpg',
     imgAlt:'tbc',
-    imgWidth:'80%',
+    imgWidth:'60%',
     imgHeight:'400',
     imgClassName:'w3-section',
-    type:'cannal',
-    length:'tbc',
+    service:'tbc',
     year:'2018',
-    containerDivClassName:'w3-container w3-center w3-black  w3-border-dark-grey w3-margin-top w3-padding-32',
-    h3className:'w3-dark-grey',
-    blackBtnClassName:'w3-button w3-black w3-text-silver w3-half',
-    dgBtnClassName:'w3-button w3-dark-grey w3-half',
+    containerDivClassName:'w3-container w3-center w3-white w3-border-dark-grey w3-margin-top w3-padding-32',
+    blackBtnClassName:'w3-button w3-black w3-text-dark-grey w3-half',
+    dgBtnClassName:'w3-button w3-dark-grey w3-half ',
     sliderBtnClassName:'w3-dark-grey w3-border-black'
 };
--- services/slider.jsx
+++ services/slider.jsx
@@ -1,32 +1,54 @@
 import {Service} from './services';
 
-const serviceList = [];
-
-for(i=1;i<41;i++){
-  serviceList.push({src: `http://res.cloudinary.com/fatso/image/upload/v1523744271/Waterflo/services/${i}.jpg`});
-  
-  
-};
+const serviceList = [
+  'http://res.cloudinary.com/fatso/image/upload/v1523744319/Waterflo/services/33.jpg',
+  'http://res.cloudinary.com/fatso/image/upload/v1523744319/Waterflo/services/28.jpg',
+  'http://res.cloudinary.com/fatso/image/upload/v1523744314/Waterflo/services/36.jpg',
+  'http://res.cloudinary.com/fatso/image/upload/v1523744312/Waterflo/services/31.jpg',
+  'http://res.cloudinary.com/fatso/image/upload/v1523744312/Waterflo/services/32.jpg',
+  'http://res.cloudinary.com/fatso/image/upload/v1523744310/Waterflo/services/35.jpg',
+  'http://res.cloudinary.com/fatso/image/upload/v1523744305/Waterflo/services/29.jpg',
+  'http://res.cloudinary.com/fatso/image/upload/v1523744302/Waterflo/services/25.jpg',
+  'http://res.cloudinary.com/fatso/image/upload/v1523744298/Waterflo/services/27.jpg',
+  'http://res.cloudinary.com/fatso/image/upload/v1523744291/Waterflo/services/26.jpg',
+  'http://res.cloudinary.com/fatso/image/upload/v1523744287/Waterflo/services/23.jpg',
+  'http://res.cloudinary.com/fatso/image/upload/v1523744285/Waterflo/services/24.jpg',
+  'http://res.cloudinary.com/fatso/image/upload/v1523744281/Waterflo/services/22.jpg',
+  'http://res.cloudinary.com/fatso/image/upload/q_auto/a_90/v1523744197/Waterflo/services/38.jpg',
+  'http://res.cloudinary.com/fatso/image/upload/v1523744274/Waterflo/services/21.jpg',
+  'http://res.cloudinary.com/fatso/image/upload/v1523744271/Waterflo/services/17.jpg',
+  'http://res.cloudinary.com/fatso/image/upload/v1523744215/Waterflo/services/39.jpg',
+  'http://res.cloudinary.com/fatso/image/upload/v1523744266/Waterflo/services/19.jpg',
+  'http://res.cloudinary.com/fatso/image/upload/v1523744266/Waterflo/services/15.jpg',
+  'http://res.cloudinary.com/fatso/image/upload/v1523744265/Waterflo/services/18.jpg',
+  'http://res.cloudinary.com/fatso/image/upload/v1523744256/Waterflo/services/16.jpg',
+  'http://res.cloudinary.com/fatso/image/upload/v1523744253/Waterflo/services/14.jpg',
+  'http://res.cloudinary.com/fatso/image/upload/v1523744249/Waterflo/services/13.jpg',
+  'http://res.cloudinary.com/fatso/image/upload/v1523744242/Waterflo/services/11.jpg',
+  'http://res.cloudinary.com/fatso/image/upload/v1523744236/Waterflo/services/40.jpg',
+  'http://res.cloudinary.com/fatso/image/upload/v1523744236/Waterflo/services/10.jpg',
+  'http://res.cloudinary.com/fatso/image/upload/a_90/v1523744231/Waterflo/services/37.jpg',
+];
 
 let num = 0;
 
-export class Slider extends React.Component{
+class Slider extends React.Component{
     next(){
-      let slider = document.getElementById("slider");
+      let slider = document.getElementById('serviceImage');
       num++;
       if(num >= serviceList.length) {
         num = 0;
       }
-      slider.src = serviceList[num].src;
+      slider.src = serviceList[num]
     }
 
     prev(){
-      let slider = document.getElementById("slider");
+      let slider = document.getElementById('serviceImage');
       num--;
       if(num < 0) {
         num = serviceList.length-1;
       }
-      slider.src = serviceList[num].src;
+      slider.src = serviceList[num]
     } 
   
     render(){      
@@ -36,7 +58,7 @@ export class Slider extends React.Component{
 
 
 
-// ReactDOM.render(
-//   <Slider />,
-//   document.getElementById('services')
-// );
+ReactDOM.render(
+  <Slider />,
+  document.getElementById('slider')
+);
